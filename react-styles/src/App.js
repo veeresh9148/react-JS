6be29d6d34eae897e999fe2441d5b1056374542a
veeresh9148 <102301570+veeresh9148@users.noreply.github.com>
2022-05-16 12:57:23 +0530

    React Stylesheets
    React Stylesheets

---
+++ react-styles/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import InLine from './components/InLine'
+import StyleSheet from './components/Stylesheet'
+function App() {
+  return (
+    <div className="App">
+     <InLine />
+     <StyleSheet />
+
+    </div>
+  );
+}
+
+export default App;
